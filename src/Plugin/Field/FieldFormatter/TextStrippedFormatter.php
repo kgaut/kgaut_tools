ff2e6cb1cd7bb0ec44056350b4b2d9a1920fc0af
Kevin Gautreau <contact@kgaut.net>
2018-01-02 16:00:00 +0100

    Add text formmater - striptags

---
+++ src/Plugin/Field/FieldFormatter/TextStrippedFormatter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\kgaut_tools\Plugin\Field\FieldFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\text\Plugin\Field\FieldFormatter\TextSummaryOrTrimmedFormatter;
+use Drupal\text\Plugin\Field\FieldFormatter\TextTrimmedFormatter;
+
+/**
+ * Plugin implementation of the 'text_summary_or_trimmed' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "text_summary_or_trimmed_then_stripped",
+ *   label = @Translation("Summary or trimmed then stripped"),
+ *   field_types = {
+ *     "text",
+ *     "text_long",
+ *     "text_with_summary"
+ *   },
+ *   quickedit = {
+ *     "editor" = "form"
+ *   }
+ * )
+ */
+class TextStrippedFormatter extends TextTrimmedFormatter {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    $render_as_summary = function (&$element) {
+      // Make sure any default #pre_render callbacks are set on the element,
+      // because text_pre_render_summary() must run last.
+      $element += \Drupal::service('element_info')->getInfo($element['#type']);
+      // Add the #pre_render callback that renders the text into a summary.
+      $element['#pre_render'][] = [TextTrimmedFormatter::class, 'preRenderSummary'];
+      // Pass on the trim length to the #pre_render callback via a property.
+      $element['#text_summary_trim_length'] = $this->getSetting('trim_length');
+    };
+
+    // The ProcessedText element already handles cache context & tag bubbling.
+    // @see \Drupal\filter\Element\ProcessedText::preRenderText()
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#type' => 'processed_text',
+        '#text' => NULL,
+        '#format' => $item->format,
+        '#langcode' => $item->getLangcode(),
+      ];
+
+      if ($this->getPluginId() === 'text_summary_or_trimmed' && !empty($item->summary)) {
+        $elements[$delta]['#text'] = strip_tags($item->summary);
+      }
+      else {
+        $elements[$delta]['#text'] = strip_tags($item->value);
+        $render_as_summary($elements[$delta]);
+      }
+    }
+
+    return $elements;
+  }
+
+}
