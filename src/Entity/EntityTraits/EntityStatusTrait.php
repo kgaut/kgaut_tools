458dbb44b8a54a31e0e732a4aff7b0566f38685f
Kevin Gautreau <contact@kgaut.net>
2018-02-13 15:16:56 +0100

    Create EntityStatusTrait

---
+++ src/Entity/EntityTraits/EntityStatusTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\kgaut_tools\Entity\EntityTraits;
+
+trait EntityStatusTrait {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isPublished() {
+    return (bool) $this->getEntityKey('status');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPublished($published) {
+    $this->set('status', $published ? TRUE : FALSE);
+    return $this;
+  }
+
+}
