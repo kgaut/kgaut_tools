0d7a3df800cd8c8eeb62030dd9febe22952a6908
Kevin Gautreau <contact@kgaut.net>
2019-06-24 12:47:04 +0200

    BodyImagePathProcess - add link to image

--- src/Plugin/migrate/process/BodyImagePathProcess.php
+++ src/Plugin/migrate/process/BodyImagePathProcess.php
@@ -101,28 +101,79 @@ class BodyImagePathProcess extends ProcessPluginBase {
       }
       $html = str_replace($sources, $destinations, $html);
     }
-/*
+
+    $result = NULL;
     //<a[^>\/]+(\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))[.]+
-    preg_match_all('/(?<=(["\']))((?<=href=[\'"])).*?(?:png|jpg|jpeg|gif|png|svg)/i', $html, $result);
-    if(!empty($result[0])) {
-      dd($result);
+    preg_match_all('/(href=")([^".]*).*?(:png|jpg|jpeg|gif|png|svg)(")/mi', $html, $result);
+    if(!empty($result[2])) {
       $i = 0;
-      dd('-----------');
       $sources = [];
       $destinations = [];
-      foreach ($result[1] as $img) {
+      foreach ($result[2] as $key => $img) {
+        $i++;
+        $img = $img . '.' . $result[3][$key];
         if (strpos($img, '/') === 0) {
-          dd($img);
-          $i++;
-          $filepath = str_replace('"', '', $img);
+          $filepath = $img;
+          // Create file object from a locally copied file.
+          $pathinfos = pathinfo($filepath);
+          $filename = $pathinfos['basename'];
 
+          if (strpos($filename, '?') > 0) {
+            $filename = substr($filename, 0, strpos($filename, '?'));
+          }
+          $path = $pathinfos['dirname'];
+          if($rename) {
+            $destination_finale = $destination . $stringCleaner->clean($row->getSourceProperty('title'));
+            $filename_destination = $stringCleaner->clean($row->getSourceProperty('title')) . '-' . urldecode($filename);
+          }
+          else {
+            $new_path = str_replace($images_source, '', $path);
+            $destination_finale = $destination . $new_path;
+            $filename_destination = urldecode($filename);
+          }
+          $new_destination = $destination_finale . '/' . $filename_destination;
+          $uri_destination = str_replace('public://', '/' . PublicStream::basePath() . '/', $new_destination);
+          if(file_exists($new_destination) && !$replace) {
+            $sources[$i] = $filepath;
+            $destinations[$i] = $uri_destination;
+            continue;
+          }
+          if (!file_prepare_directory($destination_finale, FILE_CREATE_DIRECTORY)) {
+            \Drupal::logger('migrate')->error(t('Error creating folder @folder', ['@folder' => $destination_finale]));
+            continue;
+          }
+          if (filter_var($filepath, FILTER_VALIDATE_URL)) {
+            $context = NULL;
+            if($auth) {
+              $context = stream_context_create(['http' => ['header'  => 'Authorization: Basic ' . $auth]]);
+            }
+            if($url_to_replace) {
+              $filepath = str_replace($url_to_replace, $url_source, $filepath);
+            }
+            $file_contents = file_get_contents($filepath, FALSE, $context);
+          }
+          else {
+            $context = NULL;
+            if($auth) {
+              $context = stream_context_create(['http' => ['header'  => 'Authorization: Basic ' . $auth]]);
+            }
+            $file_contents = file_get_contents($url_source . $filepath, FALSE, $context);
+          }
+          if (!$file_contents || empty($file_contents)) {
+            \Drupal::logger('migrate')->error(t('Error getting content of remote file @file', ['@file' => $file_contents]));
+          }
+          elseif ($file = file_save_data($file_contents, $new_destination, FILE_EXISTS_REPLACE)) {
+            $sources[$i] = $filepath;
+            $destinations[$i] = $uri_destination;
+          }
+          else {
+            \Drupal::logger('migrate')->error(t('Error saving file @file', ['@file' => $new_destination]));
+          }
         }
       }
-      dd($sources);
-      dd($destinations);
-      dd($row->get);
+      $html = str_replace($sources, $destinations, $html);
     }
-*/
+
     return $html;
   }
 }
