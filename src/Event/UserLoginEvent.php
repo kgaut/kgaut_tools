6806a7928e5fa0ddeae70d362e07b810ed3ad1ad
Kevin Gautreau <contact@kgaut.net>
2019-10-11 09:03:26 +0200

    Add event dispatcher on user log in

---
+++ src/Event/UserLoginEvent.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\kgaut_tools\Event;
+
+use Drupal\user\UserInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Event that is fired when a user logs in.
+ */
+class UserLoginEvent extends Event {
+
+  const EVENT_NAME = 'kgaut_tools_user_login';
+
+  /**
+   * The user account.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  public $account;
+
+  /**
+   * Constructs the object.
+   *
+   * @param \Drupal\user\UserInterface $account
+   *   The account of the user logged in.
+   */
+  public function __construct(UserInterface $account) {
+    $this->account = $account;
+  }
+
+}
